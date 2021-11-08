753e89c2a9f46692f5ddc9c7d3c53603bb15a189
Samanth <samanthms131211@gmail.com>
2021-11-09 05:18:02 +0530

    update

--- main.go
+++ main.go
@@ -2,8 +2,44 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"time"
 )
 
+func DNSlookups(url string, pointer string) {
+
+	ipRec, _ := net.LookupIP(url)
+	for _, ip := range ipRec {
+		fmt.Println("IP address is: ", ip)
+	}
+
+	canonicalName, _ := net.LookupCNAME(url)
+	fmt.Println(canonicalName)
+
+	ptr, _ := net.LookupAddr(pointer)
+	for _, ptrvalue := range ptr {
+		fmt.Println(ptrvalue)
+	}
+
+	nameServer, _ := net.LookupNS(url)
+	for _, ns := range nameServer {
+		fmt.Println(ns)
+	}
+
+	mxRecords, _ := net.LookupMX(url)
+	for _, mx := range mxRecords {
+		fmt.Println(mx.Host, mx.Pref)
+	}
+}
+
 func main() {
-	fmt.Println("testMeee")
+
+	t1 := time.Now()
+
+	t2 := time.Now()
+	diff := t2.Sub(t1)
+	fmt.Println("testMeee", diff)
+
+	DNSlookups("google.com", "8.8.8.8")
+
 }
