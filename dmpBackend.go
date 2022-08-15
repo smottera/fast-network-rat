edf0011888bc8efc9e6680eef0bd2ba961a491d3
samanth <samanthms131211@gmail.com>
2022-08-15 21:30:00 +0530

    init

---
+++ dmpBackend.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func connectToPostgres() error {
+	return nil
+}
+
+func getSurveillanceRequest() error {
+
+	return nil
+}
+
+func registerUser() error {
+	return nil
+}
+func authenticateUser() error {
+	return nil
+}
+
+func updateAccountSettings() error {
+	return nil
+}
+
+func checkRequestHistory() error {
+	return nil
+}
+
+func loadUserProperties() error {
+	return nil
+}
+
+func searchProperties() error {
+	return nil
+}
+
+func main() {
+	fmt.Println("niggus")
+}
